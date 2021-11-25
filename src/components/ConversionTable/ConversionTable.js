72157e8e254e663a1dcb5d7e5b88570d62fe141e
Povilas Medvedevas <pdmedvedevas@gmail.com>
2021-11-25 13:04:42 +0200

    trying to fetch data

--- src/components/ConversionTable/ConversionTable.js
+++ src/components/ConversionTable/ConversionTable.js
@@ -1,6 +1,19 @@
 import "./ConversionTable.scss";
+import React, { useState, useEffect } from "react";
 
 export const ConversionTable = ({ children }) => {
+  const [payCurrency, setPayCurrency] = useState("");
+
+  const fetchCurrencies = async() => {
+    const response = await fetch("https://api.coingate.com/v2/rates");
+    const result = await response.json();
+    console.log(result);
+  }
+
+  useEffect(() => {
+    fetchCurrencies();
+  },[])
+
   return (
     <div className="conversion-table">
       <div className="conversion-table conversion-table--frame"></div>
